03cdf795239b330b7f00422cadbd81a5b9ed0667
pandano6 <62196879+pandano6@users.noreply.github.com>
2020-03-15 12:17:21 +0700

    testgame

---
+++ panda/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1584249034,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/life-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/playbotton-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/logo-sheet0.png",
+		"media/mattoglseby - 1.m4a",
+		"media/mattoglseby - 1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
